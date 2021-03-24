e2bf16e2e9e60a9c43943d7e50f9f37612e1f123
zoso <benjamin.landorff@t-online.de>
2021-03-24 09:18:44 +0100

    bonusOnly modifiers & more modifier tests

--- test/modifier.test.mjs
+++ test/modifier.test.mjs
@@ -3,69 +3,83 @@ import assert from "assert"
 describe('basic modifier test', () => {
     it('empty list of modifiers', () => {
         assert.strictEqual(calculate_effective_modifiers(
-            []), 0);
+            []),
+            0);
     });
     it('enhancement bonus', () => {
         assert.strictEqual(calculate_effective_modifiers(
-            [{"value": 2, "modifierType" : "enhancement"}]), 2);
+            [{"value": 2, "modifierType" : "enhancement"}]),
+            2);
     })
     it('unknown modifier type', () => {
         assert.strictEqual(calculate_effective_modifiers(
-            [{"value": 2, "modifierType" : "grzlwimpf"}]),0);
+            [{"value": 2, "modifierType" : "grzlwimpf"}]),
+            0);
     })
     it('profane penalty', () => {
         assert.strictEqual(calculate_effective_modifiers(
-            [{"value": -2, "modifierType" : "profane"}]), -2);
+            [{"value": -2, "modifierType" : "profane"}]),
+            -2);
     })
     it('profane modifiers', () => {
         assert.strictEqual(calculate_effective_modifiers(
             [{"value": -2, "modifierType" : "profane"},
-            {"value": 3, "modifierType" : "profane"}]), 1);
+            {"value": 3, "modifierType" : "profane"}]),
+            1);
     })
     it('boni non-stacking', () => {
         assert.strictEqual(calculate_effective_modifiers(
             [{"value": 2, "modifierType" : "profane"},
-            {"value": 3, "modifierType" : "profane"}]), 3);
+            {"value": 3, "modifierType" : "profane"}]),
+            3);
     })
     it('penalties non-stacking', () => {
         assert.strictEqual(calculate_effective_modifiers(
             [{"value": -3, "modifierType" : "profane"},
-            {"value": -2, "modifierType" : "profane"}]), -3);
+            {"value": -2, "modifierType" : "profane"}]),
+            -3);
     })
     it('boni stacking (different source)', () => {
         assert.strictEqual(calculate_effective_modifiers(
             [{"value": 2, "modifierType" : "unnamed", "source" : 1},
-            {"value": 3, "modifierType" : "unnamed", "source": 2}]), 5);
+            {"value": 3, "modifierType" : "unnamed", "source": 2}]),
+            5);
     })
     it('penalties stacking (different source)', () => {
         assert.strictEqual(calculate_effective_modifiers(
             [{"value": -3, "modifierType" : "unnamed", "source" : 1},
-            {"value": -2, "modifierType" : "unnamed", "source": 2}]), -5);
+            {"value": -2, "modifierType" : "unnamed", "source": 2}]),
+            -5);
     })
     it('boni non-stacking (same source)', () => {
         assert.strictEqual(calculate_effective_modifiers(
             [{"value": 2, "modifierType" : "unnamed", "source" : 1},
-            {"value": 3, "modifierType" : "unnamed", "source": 1}]), 3);
+            {"value": 3, "modifierType" : "unnamed", "source": 1}]),
+            3);
     })
     it('penalties non-stacking (same source)', () => {
         assert.strictEqual(calculate_effective_modifiers(
             [{"value": -3, "modifierType" : "unnamed", "source" : 1},
-            {"value": -2, "modifierType" : "unnamed", "source": 1}]), -3);
+            {"value": -2, "modifierType" : "unnamed", "source": 1}]),
+            -3);
     })
     it('bonus stacking (overriding modifierType stacking)', () => {
         assert.strictEqual(calculate_effective_modifiers(
             [{"value": 2, "modifierType" : "shield", "source" : "shield"},
-            {"value": 2, "modifierType" : "shield", "stacks": true, "source" : "spell"}]), 4);
+            {"value": 2, "modifierType" : "shield", "stacks": true, "source" : "spell"}]),
+            4);
     })
     it('bonus not stacking (overriding modifierType stacking, but same source)', () => {
         assert.strictEqual(calculate_effective_modifiers(
             [{"value": 2, "modifierType" : "shield", "source" : "shield"},
-            {"value": 2, "modifierType" : "shield", "stacks": true, "source" : "shield"}]), 2);
+            {"value": 2, "modifierType" : "shield", "stacks": true, "source" : "shield"}]),
+            2);
     })
     it('penalty with bonus-only modifierType', () => {
         assert.strictEqual(calculate_effective_modifiers(
             [{"value": 2, "modifierType" : "unnamed"},
-            {"value": -2, "modifierType" : "shield"}]), 2);
+            {"value": -2, "modifierType" : "shield"}]),
+            2);
     })
     it('complex example', () => {
         assert.strictEqual(calculate_effective_modifiers(
